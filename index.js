1fe4c3b8ffa284d62a1d241dfdbaa2bc85a1ee3e
Roman <roman.koles@bk.ru>
2026-04-21 19:00:58 +0500

    done 1

---
+++ index.js
@@ -0,0 +1,23 @@
+const addButton = document.querySelector(".add-button");
+const form = document.querySelector("form");
+
+addButton.addEventListener("click", () => {
+    const beverages = document.querySelectorAll(".beverage");
+    const newIndex = beverages.length + 1;
+
+    const lastBeverage = beverages[beverages.length - 1];
+    const newBeverage = lastBeverage.cloneNode(true);
+
+    newBeverage.querySelector(".beverage-count").textContent = `Напиток №${newIndex}`;
+
+    const radioButtons = newBeverage.querySelectorAll('input[type="radio"]');
+    radioButtons.forEach((radio) => {
+        radio.name = `milk-${newIndex}`;
+        radio.checked = false;
+    });
+
+    newBeverage.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+    form.insertBefore(newBeverage, addButton.parentElement);
+});
